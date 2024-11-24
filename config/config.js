619a0cff312b215d08051246bbababcdfa889320
christian felipe morales sagredo <christianfelipemoralessagredo@MacBook-Air-de-christian.local>
2024-11-24 17:17:17 -0300

    first commit

---
+++ config/config.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    host: 'bycyqzbdadxwtqtuvaoa-mysql.services.clever-cloud.com',
+    user: 'ufk4d4narvb0tavn',
+    password: 'XU9lekbFRYrbXsOVCY79',
+    database: 'bycyqzbdadxwtqtuvaoa'
+});
+
+db.connect(function(err) {
+    if (err) throw err;
+    console.log('DATABASE CONNECTED!');
+});
+
+module.exports = db;
