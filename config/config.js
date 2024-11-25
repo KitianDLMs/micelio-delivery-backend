685f6022e68418a55601466d554b521a3c804e73
christian felipe morales sagredo <christianfelipemoralessagredo@MacBook-Air-de-christian.local>
2024-11-24 22:54:19 -0300

    FIX: fix the config.js file

--- config/config.js
+++ config/config.js
@@ -1,34 +1,15 @@
 const mysql = require('mysql');
-const dbConfig = {
-  host: 'bycyqzbdadxwtqtuvaoa-mysql.services.clever-cloud.com',
-  user: 'ufk4d4narvb0tavn',
-  password: 'XU9lekbFRYrbXsOVCY79',
-  database: 'bycyqzbdadxwtqtuvaoa'
-};
 
-let db;
+const db = mysql.createConnection({
+    host: 'bycyqzbdadxwtqtuvaoa-mysql.services.clever-cloud.com',
+    user: 'ufk4d4narvb0tavn',
+    password: 'XU9lekbFRYrbXsOVCY79',
+    database: 'bycyqzbdadxwtqtuvaoa'
+});
 
-function handleDisconnect() {
-  db = mysql.createConnection(dbConfig); // Recrear la conexión
+db.connect(function(err) {
+    if (err) throw err;
+    console.log('DATABASE CONNECTED!');
+});
 
-  db.connect(function(err) {
-    if (err) {
-      console.error('error connecting to db: ' + err.stack);
-      setTimeout(handleDisconnect, 2000); // Esperar 2 segundos antes de intentar nuevamente
-    } else {
-      console.log('connected to db');
-    }
-  });
-
-  db.on('error', function(err) {
-    console.log('DB error', err);
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-      handleDisconnect(); // Si se pierde la conexión, reconectar
-    } else {
-      throw err; // Otros errores
-    }
-  });
-}
-
-handleDisconnect(); // Iniciar la conexión
 module.exports = db;
