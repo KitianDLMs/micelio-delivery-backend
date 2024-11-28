1b3b2b0ffcdb2e95ec1fa40ece1050dd3a941634
christian felipe morales sagredo <christianfelipemoralessagredo@MacBook-Air-de-christian.local>
2024-11-28 18:23:17 -0300

    FIX: fix

--- config/config.js
+++ config/config.js
@@ -1,27 +1,28 @@
 const mysql = require('mysql');
 
-let connection;
+let db; // Cambiamos el nombre a 'db'
 
 function handleDisconnect() {
-    connection = mysql.createConnection({
+    db = mysql.createConnection({
         host: 'bycyqzbdadxwtqtuvaoa-mysql.services.clever-cloud.com',
         user: 'ufk4d4narvb0tavn',
         password: 'XU9lekbFRYrbXsOVCY79',
         database: 'bycyqzbdadxwtqtuvaoa',
     });
 
-    connection.connect(err => {
+    db.connect(err => {
         if (err) {
-            console.error('Error al reconectar:', err);
+            console.error('Error al conectar con la base de datos:', err);
             setTimeout(handleDisconnect, 2000); // Reintento después de 2 segundos
         } else {
-            console.log('Conexión restaurada');
+            console.log('¡Connected MYSQL!');
         }
     });
 
-    connection.on('error', err => {
+    db.on('error', err => {
         console.error('Error en la conexión:', err);
         if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.log('Disconnected MySQL...');
             handleDisconnect();
         } else {
             throw err;
@@ -29,4 +30,8 @@ function handleDisconnect() {
     });
 }
 
+// Inicia la conexión
 handleDisconnect();
+
+// Exporta la conexión como 'db'
+module.exports = db;
